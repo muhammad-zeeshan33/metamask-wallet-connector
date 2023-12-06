d0ab66e7258d830b52d7ca0d0d5c2a4cbc853933
Muhammad Zeeshan <muhammadzeeshan33300@gmail.com>
2023-12-06 17:03:02 +0500

    Meta Mask Connector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , NgZone} from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metamask-wallet-connector';
+
+  walletAddress: string | undefined;
+  walletBalance: string | undefined;
+
+
+  provider: ethers.providers.Web3Provider | undefined;
+  signer: ethers.providers.JsonRpcSigner | undefined;
+  connecting : boolean = false;
+  connected : boolean = false;
+
+  constructor(private zone: NgZone) {}
+
+  async connectToMetaMask() {
+    this.connecting = true;
+    try {
+      // Type casting to any to access ethereum property
+      const windowWithEthereum = window as any;
+
+      if (!windowWithEthereum.ethereum) {
+        this.connecting = false;
+        alert('MetaMask is not installed!');
+        return;
+      }
+
+      // if already connected, do nothing
+      if (this.provider) {
+        console.log('Already connected to MetaMask!');
+        this.connecting = false;
+        return;
+      }
+      await windowWithEthereum.ethereum.request({ method: 'eth_requestAccounts' });
+
+      // Use Web3Provider from ethers library
+      this.provider = new ethers.providers.Web3Provider(windowWithEthereum.ethereum);
+
+      this.signer = this.provider.getSigner();
+
+      windowWithEthereum.ethereum.on('accountsChanged', (accounts: string[]) => {
+        this.zone.run(() => {
+          this.signer = this.provider?.getSigner();
+        });
+      });
+
+      windowWithEthereum.ethereum.on('chainChanged', (chainId: string) => {
+        this.zone.run(() => {
+          // Handle chain changes if needed
+        });
+      });
+
+      this.connecting = false;
+      this.connected = true;
+      
+      // get Wallet info from signer
+      const address = await this.signer.getAddress();                  
+      this.walletAddress = address;
+
+      // get current balance
+      const balance = await this.provider.getBalance(address);      
+      this.walletBalance = ethers.utils.formatEther(balance);    
+
+    } catch (error) {
+      this.connecting = false;
+      this.connected = false;
+      console.error('Error connecting to MetaMask:', error);
+    }
+  }
+
 }
